ab3ec0e02baaa8670046f2b70f985c90705c918d
Tim Haines <thaines.astro@gmail.com>
2024-11-23 01:20:32 -0600

    Remove test 6
    It's only valid on 32-bit x86. The Dyninst decoder incorrectly allowed
    it.

--- instructionAPI/syscall-x86.cpp
+++ instructionAPI/syscall-x86.cpp
@@ -6,8 +6,8 @@
 
 namespace di = Dyninst::InstructionAPI;
 
-constexpr auto num_tests = 15;
-constexpr auto num_bytes = 63UL;
+constexpr auto num_tests = 14;
+constexpr auto num_bytes = 62UL;
 std::array<const unsigned char, num_bytes> buffer = {
   0xe8, 0x10, 0x00, 0x00, 0x00,                     // (1)  call 0x10 + rip
   0x64, 0xff, 0x14, 0x25, 0x10, 0x00, 0x00, 0x00,   // (2)  call  [q|d]word ptr fs:[0x10]
@@ -19,11 +19,10 @@ std::array<const unsigned char, num_bytes> buffer = {
   0x65, 0xff, 0x50, 0x10,                           // (8)  call gs:[rax + 0x10]
   0xcd, 0x80,                                       // (9)  int 0x80
   0xcc,                                             // (10) int3
-  0xce,                                             // (11) into
-  0xf1,                                             // (12) int1
-  0x0f, 0x05,                                       // (13) syscall
-  0x0f, 0x34,                                       // (14) sysenter
-  0xcd, 0x0a,                                       // (15) int 0x0a
+  0xf1,                                             // (11) int1
+  0x0f, 0x05,                                       // (12) syscall
+  0x0f, 0x34,                                       // (13) sysenter
+  0xcd, 0x0a,                                       // (14) int 0x0a
 };
 
 bool run_32() {
@@ -39,10 +38,9 @@ bool run_32() {
     true,     // (9)
     false,    // (10)
     false,    // (11)
-    false,    // (12)
-    true,     // (13)
-    false,    // (14)
-    true,     // (15)
+    true,     // (12)
+    false,    // (13)
+    true,     // (14)
   };
 
   di::InstructionDecoder decoder(buffer.data(), buffer.size(), Dyninst::Arch_x86);
@@ -76,10 +74,9 @@ bool run_64()  {
     true,     // (9)
     false,    // (10)
     false,    // (11)
-    false,    // (12)
-    true,     // (13)
-    false,    // (14)
-    true,     // (15)
+    true,     // (12)
+    false,    // (13)
+    true,     // (14)
   };
 
   di::InstructionDecoder decoder(buffer.data(), buffer.size(), Dyninst::Arch_x86_64);
