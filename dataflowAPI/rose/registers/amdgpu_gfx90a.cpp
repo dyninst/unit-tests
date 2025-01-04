a56cd46e6292b06c960b1aeecd010fafc80b0ee6
Tim Haines <thaines.astro@gmail.com>
2025-01-04 09:01:50 -0600

    Add amdgpu stubs

---
+++ dataflowAPI/rose/registers/amdgpu_gfx90a.cpp
@@ -0,0 +1,7 @@
+#include "dataflowAPI/rose/registers/convert.h"
+#include "external/rose/rose-compat.h"
+#include "registers/AMDGPU/amdgpu_gfx90a_regs.h"
+#include "rose_reg_check.h"
+
+int main() {
+}
